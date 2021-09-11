96a071db062a52673ddc4901737c866b42412048
Dai <let__it__be@hb.tp1.jp>
2021-09-11 23:06:13 +0900

    first commit

---
+++ js/main.js
@@ -0,0 +1,10 @@
+$(function(){
+    $('.heroSlide').slick({
+        autoplay: true,
+        autoplaySpeed: 3000,
+        dots: true,
+        fade: true,
+        arrows: false,
+	    infinite: true,
+    });
+});
